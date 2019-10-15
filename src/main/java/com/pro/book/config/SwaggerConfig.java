ebb745ba874188ab937401925dd3aa78e2b5e1a1
program_fighters <jha.sanjeev69@gmail.com>
2019-10-15 22:51:40 +0530

    add swager config

--- src/main/java/com/pro/book/config/SwaggerConfig.java
+++ src/main/java/com/pro/book/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.pro.book.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,10 +14,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-  @Bean
-  public Docket productApi() {
-    return new Docket(DocumentationType.SWAGGER_2).select()
-        .apis(RequestHandlerSelectors.basePackage("com.pro.book")).build();
-  }
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).
+                apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.pro.book"))
+                .paths(PathSelectors.any()).build();
 
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Online Book management store api",
+                "Book store",
+                "1.0.0",
+                "Terms of service",
+                new Contact("developer", "www.apanakaam.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Lists.newArrayList());
+    }
 }
--- src/main/java/com/pro/book/models/Book.kt
+++ src/main/java/com/pro/book/models/Book.kt
@@ -1,6 +1,12 @@
 package com.pro.book.models
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
+@ApiModel("This is Book Model")
 data class Book(
+        @ApiModelProperty(value = "name of book")
         var name: String,
+        @ApiModelProperty(value = "price of book")
         var price: Long
 )
